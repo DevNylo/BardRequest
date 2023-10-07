8d35e9a105763e0e5044048fc5f55b093cbc63ad
Danilo Rocha <contato_dsr@hotmail.com>
2023-10-07 01:31:29 -0300

    alterações nas variaveis

--- src/main/java/com/devnylo/api/BardRequest.java
+++ src/main/java/com/devnylo/api/BardRequest.java
@@ -7,7 +7,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class HumanizeMe {
+public class BardRequest {
     public static void main(String[] args) throws IOException, InterruptedException {
 
 
@@ -17,24 +17,27 @@ public class HumanizeMe {
         // O ENCONDER QUE VAI TRANSFORMAR A PERGUNTA EM UM PADRÃO HTTPS URL.
         String codificarPergunta = URLEncoder.encode(pergunta, "UTF-8");
 
-        // COOKIE 1PSID
+        // CONSTANTE DESTINADA AO COOKIE 1PSID
         final String _1PSID = "";
 
-        // COOKIE 1PSIDTS
+        // CONSTANTE DESTINADA AO COOKIE 1PSIDTS
         final String _1PSIDTS = "";
 
-        // COOKIE 1PSIDCC
+        // CONSTANTE DESTINADA AO COOKIE 1PSIDCC
         final String _1PSIDCC = "";
+
+        // CONSTANTE DESTINADA A API KEY
+        final String APIKEY = "";
 
         // ALGORITMO QUE IRÁ FAZER A REQUISIÇÃO A API.
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://google-bard-api1.p.rapidapi.com/ask?question="+codificarPergunta+
+                .uri(URI.create(
+                        "https://google-bard-api1.p.rapidapi.com/ask?question="+codificarPergunta+
                         "&bard___Secure-1PSID_cookie_value="+_1PSID+
                         "&bard___Secure-1PSIDTS_cookie_value="+_1PSIDTS+
                         "&bard___Secure-1PSIDCC_cookie_value="+_1PSIDCC))
 
-                // ADICIONE SUA API KEY
-                .header("X-RapidAPI-Key", "")
+                .header("X-RapidAPI-Key", APIKEY)
                 .header("X-RapidAPI-Host", "google-bard-api1.p.rapidapi.com")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
