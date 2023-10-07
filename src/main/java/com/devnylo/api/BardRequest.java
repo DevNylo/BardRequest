b891e17750f51ddfeb1a5cda5a82e2f1a2ec9c60
Danilo Rocha <contato_dsr@hotmail.com>
2023-10-07 14:01:37 -0300

    Resposta tratada

--- src/main/java/com/devnylo/api/BardRequest.java
+++ src/main/java/com/devnylo/api/BardRequest.java
@@ -7,27 +7,30 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class BardRequest {
     public static void main(String[] args) throws IOException, InterruptedException {
 
 
         // VARIÁVEL QUE VAI ARMAZENAR A PERGUNTA.
-        String pergunta = "";
+        String pergunta = "Diga Olá!";
 
         // O ENCONDER QUE VAI TRANSFORMAR A PERGUNTA EM UM PADRÃO HTTPS URL.
         String codificarPergunta = URLEncoder.encode(pergunta, "UTF-8");
 
         // CONSTANTE DESTINADA AO COOKIE 1PSID
-        final String _1PSID = "";
+        final String _1PSID = "bwifxaLbTj6p08FftDY0VNVRxCft9lRQ-nkWBAfocj2ej1Lb8qw2rpswLxSC8XveZLcR1w.";
 
         // CONSTANTE DESTINADA AO COOKIE 1PSIDTS
-        final String _1PSIDTS = "";
+        final String _1PSIDTS = "sidts-CjIB3e41hbGsr9xJgqJiiGwvWE7y3xN9WJ0D6TifWWQS6gS5cUdYydxMb1Otv4Xka4BtexAA";
 
         // CONSTANTE DESTINADA AO COOKIE 1PSIDCC
-        final String _1PSIDCC = "";
+        final String _1PSIDCC = "ACA-OxOMTZLLozzRhJUACGphNQ1Ck-KxHrYYRghlzVCNGysHDa7GbCDLEmAAecjZeqrpV_Uu5Q";
 
         // CONSTANTE DESTINADA A API KEY
-        final String APIKEY = "";
+        final String APIKEY = "de0a7bf1a0mshc9c86775ad2a249p16fbc3jsn9ba083d658f4";
 
         // ALGORITMO QUE IRÁ FAZER A REQUISIÇÃO A API.
         HttpRequest request = HttpRequest.newBuilder()
@@ -44,6 +47,21 @@ public class BardRequest {
 
         // RESPOSTA RECEBIDA E EXIBIDA NO CONSOLE, TRATE DA FORMA QUE ACHAR NECESSÁRIO.
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        // ANALISE DA RESPOSTA JSON
+        JSONObject jsonResponse = new JSONObject(response.body());
+
+        // EXTRÇÃO DO VALOR DA PALAVRA CHAVE "RESPONSE"
+        JSONObject responseObject = jsonResponse.getJSONObject("response");
+
+        // Imprime o valor da chave "response"
+        //System.out.println(responseObject);
+
+        // RESPOSTA TRATADA :)
+        JSONArray draftsArray = responseObject.getJSONArray("drafts");
+        System.out.println(draftsArray.getJSONArray(1).getString(0));
+
+
+
     }
 }
